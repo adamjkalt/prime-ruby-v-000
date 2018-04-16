dab9319aa45890ec7760cdd54274119d70c68b38
Adam Kalt <adam.kalt.1@gmail.com>
2018-04-16 17:43:28 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+def prime?(integer)
+  counter = 2
+  until counter == integer
+    counter +=1
+  end
+end
+
+
+    
+  loop do
+    counter +=1
+    if integer % counter == 0
+      false
+    elsif counter == integer
+      return true
+      break
+    end
+  end
+end
